12470e04eb3ae2847e97f7c3b50d9e391d18f68d
ricardoassink <ric.assink@gmail.com>
2025-03-17 13:16:37 -0300

    Início do Projeto 2

--- src/testenetbeans/TesteNetbeans.java
+++ src/testenetbeans/TesteNetbeans.java
@@ -10,7 +10,7 @@ public class TesteNetbeans {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO llll
     }
     
 }
